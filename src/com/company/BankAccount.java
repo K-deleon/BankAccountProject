f77fa99f2eb6c7bfc6598fa49b7f9be0264be217
Kiara <deleon_k@ymail.com>
2017-10-04 08:51:18 -0400

    Update BankAccount.java

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -5,6 +5,35 @@ package com.company;
  */
 public class BankAccount {
 
+    public double balance;
+    public String name = "";
+    public double deposit;
+    public double withdraw;
 
+
+    public BankAccount(String n, double b){
+
+        balance = b;
+        name = n;
+
+    }
+
+    public void Deposit(double d){
+
+        deposit = d;
+        balance+= d;
+
+    }
+
+    public void Withdraw(double w){
+
+        w = withdraw;
+        balance-= w;
+
+    }
+    
+    public double myAccount(){
+        
+    }
 
 }
